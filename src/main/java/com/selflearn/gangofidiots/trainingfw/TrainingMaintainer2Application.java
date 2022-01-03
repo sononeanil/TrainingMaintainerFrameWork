8f0d0853e15fc04b65f00591b8a2c90606ea3466
sononeanil <anilsonone@gmail.com>
2022-01-03 22:22:51 +0530

    Inital commit

---
+++ src/main/java/com/selflearn/gangofidiots/trainingfw/TrainingMaintainer2Application.java
@@ -0,0 +1,13 @@
+package com.selflearn.gangofidiots.trainingfw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingMaintainer2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingMaintainer2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/selflearn/gangofidiots/trainingfw/TrainingMaintainer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selflearn.gangofidiots.trainingfw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingMaintainer2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
